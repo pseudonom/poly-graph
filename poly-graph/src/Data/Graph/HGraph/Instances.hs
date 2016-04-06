ef1ce1db3720d9d98e7c6143cddc5164fc720202
Eric Easley <eric@frontrowed.com>
2016-04-05 21:51:36 -0700

    Move instances into own file

---
+++ poly-graph/src/Data/Graph/HGraph/Instances.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# OPTIONS_GHC -fno-warn-incomplete-patterns #-}
+
+module Data.Graph.HGraph.Instances where
+
+import Data.Functor.Identity
+import Data.Proxy
+import Data.Tagged
+
+import Data.Graph.HGraph
+
+type Never = Proxy
+type Always = Identity
+
+pattern Always a = Identity a
+pattern Never = Proxy
+
+instance (a `PointsAt` Maybe b) => a `PointsAt` Always b where
+  a `pointsAt` Always b = a `pointsAt` (Just b :: Maybe b)
+instance (a `PointsAt` Maybe b) => a `PointsAt` Never b where
+  a `pointsAt` Never = a `pointsAt` (Nothing :: Maybe b)
+
+instance {-# OVERLAPPABLE #-} (Functor f, a `PointsAt` b) => f a `PointsAt` b where
+  fa `pointsAt` b = (`pointsAt` b) <$> fa
+
+-- The above don't compose nicely because none is 'more specific' than the other.
+-- So we write the combinations out by hand to increase specificity
+instance {-# OVERLAPPING #-} (Functor f, a `PointsAt` Maybe b) => f a `PointsAt` Always b where
+  fa `pointsAt` Always b = (`pointsAt` (Just b :: Maybe b)) <$> fa
+instance {-# OVERLAPPING #-} (Functor f, a `PointsAt` Maybe b) => f a `PointsAt` Never b where
+  fa `pointsAt` Never = (`pointsAt` (Nothing :: (Maybe b))) <$> fa
+instance {-# OVERLAPPING #-} (Functor f, a `PointsAt` Maybe b) => f a `PointsAt` Maybe b where
+  fa `pointsAt` b = (`pointsAt` b) <$> fa
+
+instance {-# OVERLAPPABLE #-} (a `PointsAt` b) => a `PointsAt` Maybe b where
+  a `pointsAt` Just b = a `pointsAt` b
+  a `pointsAt` Nothing = a
+
+-- The underlying HGraph uses @Node@s. This instance translates @Node@s into something slightly friendlier.
+instance {-# OVERLAPPING #-} (a `PointsAt` b) => Tagged t a `PointsAt` Tagged u b where
+  Tagged a `pointsAt` Tagged b = Tagged $ a `pointsAt` b
