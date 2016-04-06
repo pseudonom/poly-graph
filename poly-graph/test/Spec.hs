717e4589a33238300c4d197972c51e9fbd6b10ab
Eric Easley <eric@frontrowed.com>
2016-04-05 23:03:03 -0700

    Clean up spec

--- poly-graph/test/Spec.hs
+++ poly-graph/test/Spec.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE KindSignatures #-}
@@ -8,62 +9,48 @@
 
 import Test.Hspec
 
+import Data.Tagged
+
 import Data.Graph.HGraph
 import Data.Graph.HGraph.Instances
 import Data.Graph.HGraph.Internal (HGraph(Cons))
 
-data SelfNode
-  = SelfNode
-  { id' :: Int
-  , pointer :: Maybe Int
+data Typ = Self | A | B | C
+data Node1 (self :: Typ) (other :: Typ)
+  = Node1
+  { ident1 :: Tagged self Int
+  , pointer :: Maybe (Tagged other Int)
   } deriving (Show, Eq)
-
-newtype IdA = IdA Int deriving (Show, Eq, Num)
-data NodeA
-  = NodeA
-  { idA :: IdA
-  , aPointer :: Maybe IdB
+data Node2 (self :: Typ) (other1 :: Typ) (other2 :: Typ)
+  = Node2
+  { ident2 :: Tagged self Int
+  , pointer1 :: Maybe (Tagged other1 Int)
+  , pointer2 :: Maybe (Tagged other2 Int)
   } deriving (Show, Eq)
-
-newtype IdB = IdB Int deriving (Show, Eq, Num)
-data NodeB
-  = NodeB
-  { idB :: IdB
-  , bPointer :: Maybe IdC
-  } deriving (Show, Eq)
-
-newtype IdC = IdC Int deriving (Show, Eq, Num)
-data NodeC
-  = NodeC
-  { idC :: IdC
-  , cPointer1 :: Maybe IdA
-  , cPointer2 :: Maybe IdB
-  } deriving (Show, Eq)
-
 
 main :: IO ()
 main = hspec $ do
   let -- Make sure we support all combinations
-      node = SelfNode 1 (Just 1)
+      node = Node1 (Tagged 1) (Just $ Tagged 1) :: Node1 'Self 'Self
       plainToPlain = node `pointsAt` node
       plainToMaybe = node `pointsAt` Just node
       plainToAlways = node `pointsAt` Always node
-      plainToNever = node `pointsAt` (Never :: Never SelfNode)
+      plainToNever = node `pointsAt` (Never :: Never (Node1 'Self 'Self))
 
       maybeToPlain = Just node `pointsAt` node
       maybeToMaybe = Just node `pointsAt` Just node
       maybeToAlways = Just node `pointsAt` Always node
-      maybeToNever = Just node `pointsAt` (Never :: Never SelfNode)
+      maybeToNever = Just node `pointsAt` (Never :: Never (Node1 'Self 'Self))
 
       alwaysToPlain = Always node `pointsAt` node
       alwaysToMaybe = Always node `pointsAt` Just node
       alwaysToAlways = Always node `pointsAt` Always node
-      alwaysToNever = Always node `pointsAt` (Never :: Never SelfNode)
+      alwaysToNever = Always node `pointsAt` (Never :: Never (Node1 'Self 'Self))
 
-      neverToPlain = (Never :: Never SelfNode) `pointsAt` node
-      neverToMaybe = (Never :: Never SelfNode) `pointsAt` Just node
-      neverToAlways = (Never :: Never SelfNode) `pointsAt` Always node
-      neverToNever = (Never :: Never SelfNode) `pointsAt` (Never :: Never SelfNode)
+      neverToPlain = (Never :: Never (Node1 'Self 'Self)) `pointsAt` node
+      neverToMaybe = (Never :: Never (Node1 'Self 'Self)) `pointsAt` Just node
+      neverToAlways = (Never :: Never (Node1 'Self 'Self)) `pointsAt` Always node
+      neverToNever = (Never :: Never (Node1 'Self 'Self)) `pointsAt` (Never :: Never (Node1 'Self 'Self))
 
   describe "~>" $ do
     it "works for simple chains" $
@@ -75,39 +62,39 @@ main = hspec $ do
     it "works for a complicated mess" $
       inAndOut `shouldBe` inAndOut'
 
-instance SelfNode `PointsAt` SelfNode where
-  (SelfNode id1 _) `pointsAt` (SelfNode id2 _) = SelfNode id1 (Just id2)
-instance NodeA `PointsAt` NodeB where
-  (NodeA ida _) `pointsAt` (NodeB idb _) = NodeA ida (Just idb)
-instance NodeB `PointsAt` NodeC where
-  (NodeB idb _) `pointsAt` (NodeC idc _ _) = NodeB idb (Just idc)
-instance NodeC `PointsAt` NodeA where
-  (NodeC idc _ idb) `pointsAt` (NodeA ida _) = NodeC idc (Just ida) idb
-instance NodeC `PointsAt` NodeB where
-  (NodeC idc ida _) `pointsAt` (NodeB idb _) = NodeC idc ida (Just idb)
+instance Node1 'Self 'Self `PointsAt` Node1 'Self 'Self where
+  (Node1 id1 _) `pointsAt` (Node1 id2 _) = Node1 id1 (Just id2)
+instance Node1 'A 'B `PointsAt` Node1 'B 'C where
+  (Node1 ida _) `pointsAt` (Node1 idb _) = Node1 ida (Just idb)
+instance Node1 'B 'C `PointsAt` Node2 'C 'A 'B where
+  (Node1 idb _) `pointsAt` (Node2 idc _ _) = Node1 idb (Just idc)
+instance Node2 'C 'A 'B `PointsAt` Node1 'A 'B where
+  (Node2 idc _ idb) `pointsAt` (Node1 ida _) = Node2 idc (Just ida) idb
+instance Node2 'C 'A 'B `PointsAt` Node1 'B 'C where
+  (Node2 idc ida _) `pointsAt` (Node1 idb _) = Node2 idc ida (Just idb)
 
 simpleChain ::
   Tree (
-    NodeA :<
-      NodeB :<
-        NodeC
+    Node1 'A 'B :<
+      Node1 'B 'C :<
+        Node2 'C 'A 'B
   )
 simpleChain =
-  NodeA 1 (Just 6) ~>
-    NodeB 2 Nothing ~>
-      NodeC 3 Nothing Nothing ~>
+  Node1 1 (Just 6) ~>
+    Node1 2 Nothing ~>
+      Node2 3 Nothing Nothing ~>
         Nil
 
 simpleChain' ::
   HGraph
-  [ '(NodeA, 0, '[1])
-  , '(NodeB, 1, '[2])
-  , '(NodeC, 2, '[])
+  [ '(Node1 'A 'B, 0, '[1])
+  , '(Node1 'B 'C, 1, '[2])
+  , '(Node2 'C 'A 'B, 2, '[])
   ]
 simpleChain' =
-  Node (NodeA 1 (Just 2)) `Cons`
-    Node (NodeB 2 (Just 3)) `Cons`
-      Node (NodeC 3 Nothing Nothing) `Cons`
+  Node (Node1 1 (Just 2)) `Cons`
+    Node (Node1 2 (Just 3)) `Cons`
+      Node (Node2 3 Nothing Nothing) `Cons`
         Nil
 
 -- | Graph looks like
@@ -122,32 +109,32 @@ simpleChain' =
 -- @
 fanOut ::
   Tree (
-    NodeC :<
-      ( NodeA
-      , NodeB :< NodeC
+    Node2 'C 'A 'B :<
+      ( Node1 'A B
+      , Node1 'B 'C :< Node2 'C 'A B
       )
   )
 fanOut =
-  NodeC 1 (Just 4) Nothing ~>
-    NodeA 2 Nothing ~>
-    NodeB 3 Nothing ~>
-      NodeC 4 Nothing Nothing ~> Nil
+  Node2 1 (Just 4) Nothing ~>
+    Node1 2 Nothing ~>
+    Node1 3 Nothing ~>
+      Node2 4 Nothing Nothing ~> Nil
 
 fanOut' ::
   HGraph
-  [ '(NodeC, 0, '[100, 200])
-  , '(NodeA, 100, '[])
-  , '(NodeB, 200, '[201])
-  , '(NodeC, 201, '[])
+  [ '(Node2 'C 'A 'B, 0, '[100, 200])
+  , '(Node1 'A 'B, 100, '[])
+  , '(Node1 'B 'C, 200, '[201])
+  , '(Node2 'C 'A 'B, 201, '[])
   ]
 fanOut' =
-  Node (NodeC 1 (Just 2) (Just 3)) `Cons`
-    Node (NodeA 2 Nothing) `Cons`
-    Node (NodeB 3 (Just 4)) `Cons`
-      Node (NodeC 4 Nothing Nothing) `Cons` Nil
+  Node (Node2 1 (Just 2) (Just 3)) `Cons`
+    Node (Node1 2 Nothing) `Cons`
+    Node (Node1 3 (Just 4)) `Cons`
+      Node (Node2 4 Nothing Nothing) `Cons` Nil
 
 -- | Our "read-only" patterns work as expected
-deconstruct :: (NodeC, NodeA, NodeB, NodeC)
+deconstruct :: (Node2 'C 'A 'B, Node1 'A 'B, Node1 'B 'C, Node2 'C 'A B)
 deconstruct =
   case tree fanOut of
     c1 :< (a, b :< c2) -> (c1, a, b, c2)
@@ -165,29 +152,29 @@ deconstruct =
 
 fanIn ::
   HGraph
-  [ '(NodeC, "firstC", '["b"])
-  , '(NodeA, "a", '["b"])
-  , '(NodeB, "b", '["secondC"])
-  , '(NodeC, "secondC", '[])
+  [ '(Node2 'C 'A 'B, "firstC", '["b"])
+  , '(Node1 'A 'B, "a", '["b"])
+  , '(Node1 'B 'C, "b", '["secondC"])
+  , '(Node2 'C 'A 'B, "secondC", '[])
   ]
 fanIn =
-  NodeC 1 Nothing Nothing ~>
-  NodeA 2 (Just 1) ~>
-  NodeB 3 (Just 7) ~>
-  NodeC 4 Nothing Nothing ~> Nil
+  Node2 1 Nothing Nothing ~>
+  Node1 2 (Just 1) ~>
+  Node1 3 (Just 7) ~>
+  Node2 4 Nothing Nothing ~> Nil
 
 fanIn' ::
   HGraph
-  [ '(NodeC, "firstC", '["b"])
-  , '(NodeA, "a", '["b"])
-  , '(NodeB, "b", '["secondC"])
-  , '(NodeC, "secondC", '[])
+  [ '(Node2 'C 'A 'B, "firstC", '["b"])
+  , '(Node1 'A 'B, "a", '["b"])
+  , '(Node1 'B 'C, "b", '["secondC"])
+  , '(Node2 'C 'A 'B, "secondC", '[])
   ]
 fanIn' =
-  Node (NodeC 1 Nothing (Just 3)) `Cons`
-  Node (NodeA 2 (Just 3)) `Cons`
-  Node (NodeB 3 (Just 4)) `Cons`
-  Node (NodeC 4 Nothing Nothing) `Cons` Nil
+  Node (Node2 1 Nothing (Just 3)) `Cons`
+  Node (Node1 2 (Just 3)) `Cons`
+  Node (Node1 3 (Just 4)) `Cons`
+  Node (Node2 4 Nothing Nothing) `Cons` Nil
 
 -- | Graph looks like
 -- @
@@ -205,40 +192,40 @@ fanIn' =
 -- @
 inAndOut ::
   HGraph
-  [ '(SelfNode, 1, '[7])
-  , '(SelfNode, 2, '[3, 5, 6])
-  , '(SelfNode, 3, '[4])
-  , '(SelfNode, 4, '[7])
-  , '(SelfNode, 5, '[7])
-  , '(SelfNode, 6, '[])
-  , '(SelfNode, 7, '[])
+  [ '(Node1 'Self 'Self, 1, '[7])
+  , '(Node1 'Self 'Self, 2, '[3, 5, 6])
+  , '(Node1 'Self 'Self, 3, '[4])
+  , '(Node1 'Self 'Self, 4, '[7])
+  , '(Node1 'Self 'Self, 5, '[7])
+  , '(Node1 'Self 'Self, 6, '[])
+  , '(Node1 'Self 'Self, 7, '[])
   ]
 inAndOut =
-  SelfNode 1 Nothing ~>
-  SelfNode 2 Nothing ~>
-  SelfNode 3 Nothing ~>
-  SelfNode 4 Nothing ~>
-  SelfNode 5 Nothing ~>
-  SelfNode 6 Nothing ~>
-  SelfNode 7 Nothing ~>
+  Node1 1 Nothing ~>
+  Node1 2 Nothing ~>
+  Node1 3 Nothing ~>
+  Node1 4 Nothing ~>
+  Node1 5 Nothing ~>
+  Node1 6 Nothing ~>
+  Node1 7 Nothing ~>
   Nil
 
 inAndOut' ::
   HGraph
-  [ '(SelfNode, 1, '[7])
-  , '(SelfNode, 2, '[3, 5, 6])
-  , '(SelfNode, 3, '[4])
-  , '(SelfNode, 4, '[7])
-  , '(SelfNode, 5, '[7])
-  , '(SelfNode, 6, '[])
-  , '(SelfNode, 7, '[])
+  [ '(Node1 'Self 'Self, 1, '[7])
+  , '(Node1 'Self 'Self, 2, '[3, 5, 6])
+  , '(Node1 'Self 'Self, 3, '[4])
+  , '(Node1 'Self 'Self, 4, '[7])
+  , '(Node1 'Self 'Self, 5, '[7])
+  , '(Node1 'Self 'Self, 6, '[])
+  , '(Node1 'Self 'Self, 7, '[])
   ]
 inAndOut' =
-  Node (SelfNode 1 (Just 7)) `Cons`
-  Node (SelfNode 2 (Just 6)) `Cons`
-  Node (SelfNode 3 (Just 4)) `Cons`
-  Node (SelfNode 4 (Just 7)) `Cons`
-  Node (SelfNode 5 (Just 7)) `Cons`
-  Node (SelfNode 6 Nothing) `Cons`
-  Node (SelfNode 7 Nothing) `Cons`
+  Node (Node1 1 (Just 7)) `Cons`
+  Node (Node1 2 (Just 6)) `Cons`
+  Node (Node1 3 (Just 4)) `Cons`
+  Node (Node1 4 (Just 7)) `Cons`
+  Node (Node1 5 (Just 7)) `Cons`
+  Node (Node1 6 Nothing) `Cons`
+  Node (Node1 7 Nothing) `Cons`
   Nil
