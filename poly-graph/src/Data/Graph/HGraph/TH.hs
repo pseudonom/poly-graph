aa9f313c8ce2e4f2b38f87e057bc705973eb4774
Eric Easley <eric@frontrowed.com>
2016-05-19 18:09:08 -0700

    Add `Proxy :: Proxy "foo"` shorthand

--- poly-graph/src/Data/Graph/HGraph/TH.hs
+++ poly-graph/src/Data/Graph/HGraph/TH.hs
@@ -4,6 +4,7 @@ module Data.Graph.HGraph.TH where
 import Language.Haskell.TH
 
 import Data.Graph.HGraph
+import Data.Proxy
 
 declareBases :: [Name] -> DecsQ
 declareBases = fmap concat . mapM declareBase
@@ -11,9 +12,15 @@ declareBases = fmap concat . mapM declareBase
 declareBase :: Name -> DecsQ
 declareBase name' =
   [d|
-    type instance Base $(name) = $(name)
-    instance ToBase $(name) where
+    type instance Base $name = $name
+    instance ToBase $name where
       base = id
   |]
   where
     name = conT name'
+
+-- | Shorthand until we get explicit type application
+sym :: String -> ExpQ
+sym x' = [| Proxy :: Proxy $x |]
+  where
+    x = litT (strTyLit x')
